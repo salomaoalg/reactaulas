d40c755b61675d31a66de323e4b299bc1388a940
salomaoalg <sa.salomao@hotmail.com>
2024-03-20 23:26:04 +0000

    update form
    useState hulk

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,44 @@
-function Form(){
+import { useState } from "react"
 
-    function CadastroUsuario(e){
+function Form() {
+
+    function CadastroUsuario(e) {
         e.preventDefault()
         alert("USUARIO CADASTRADO!!")
+        alert(`Usuário ${name} foi cadastrado com a senha ${passwd}`)
     }
 
-    return(
+    const [name, setName] = useState("Matheus")
+    const [passwd, setPasswd] = useState()
+
+
+    return (
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={CadastroUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome:" />
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite seu nome:"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                </div>                
+                
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite a sua senha" 
+                        onChange={(e) => setPasswd(e.target.value)}
+                    />
                 </div>
+
                 <div>
                     <input type="submit" value="Cadastrar" />
                 </div>
