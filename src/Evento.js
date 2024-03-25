58a8b7b61e3e7f57bbd5e93082bbc5043a44fff7
salomaoalg <sa.salomao@hotmail.com>
2024-03-25 20:06:47 +0000

    eventos separados do pai
    eventos passado via props

--- src/Evento.js
+++ src/Evento.js
@@ -1,12 +1,20 @@
+import Button from "./components/Evento/Button"
+
 function Evento({numero}){
 
     function meuEvento(){
         alert(`Numero Solicitado, ${numero}`)
     }
 
+    function segundoEvento(){
+        alert(`Segundo Evento`)
+    }
+
     return(
         <>
             <p>Clique para disparar um evento!!!</p>
+            <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={segundoEvento} text="Segundo Evento"/>
             <button onClick={meuEvento}>Ativar</button>
         </>
     )
