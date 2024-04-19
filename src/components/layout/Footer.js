5f43777e29e401feb95f9f9208fbb578ebd027a2
salomaoalg <sa.salomao@hotmail.com>
2024-04-19 20:56:48 +0100

    icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,16 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa"
+import styles from "./footer.module.css"
+
 function Footer(){
     return(
-        <p>Todos os direitos reservados!</p>
+        <footer>
+            <p>Todos os direitos reservados!</p>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+        </footer>
     )
 }
 export default Footer
